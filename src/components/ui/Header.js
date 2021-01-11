f99d6c7c58d4e113db655ea2ea47e4c0a54305d5
mitushaa <aryamitusha@gmail.com>
2021-01-11 16:03:19 +0530

    Section Cleanup- Adding routes

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -156,30 +156,19 @@ export default function Header(props) {
     setOpenMenu(false)
   };
 
-  const menuOptions = [{name: "Services", link: "/services"}, {name: "Customer Software Development", link: "/customsoftware"}, 
-  {name: "Mobile App Development", link: "/mobileapps"
-  }, {name: "Website Development", link: "/websites"}]
-  
-  useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0)
-    } else if (window.location.pathname === "/services" && value !== 
-    1) {
-      setValue(1) 
-    } else if (window.location.pathname === "/revolution" && value 
-    !== 2) {
-      setValue(2)
-    } else if (window.location.pathname === "/about" && value 
-    !== 3) {
-      setValue(3)
-    } else if (window.location.pathname === "/contact" && value 
-    !== 4) {
-      setValue(4)
-    } else if (window.location.pathname === "/estimate" && value 
-    !== 5) {
-      setValue(5)
-    }
+  const menuOptions = [{name: "Services", link: "/services"}, 
+  {name: "Customer Software Development", link: "/customsoftware"}, 
+  {name: "Mobile App Development", link: "/mobileapps"}, 
+  {name: "Website Development", link: "/websites"}];
 
+  const routes = [
+    { name: "Services", link: "/services"},
+    { name: "Customer Software Development", link: "/customsoftware"},
+    { name: "Mobile App Development", link: "/mobileapps"},
+    { name: "Website Development", link: "/websites"}
+    ];
+
+  useEffect(() => {
     switch (window.location.pathname) {
       case "/":
         if (value !== 0) {
