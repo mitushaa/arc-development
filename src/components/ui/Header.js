5083fd681cabf92ba759d500041efd9be801ab01
mitushaa <aryamitusha@gmail.com>
2021-01-11 15:20:56 +0530

    Using Lists- Adding opacity

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -112,7 +112,8 @@ drawer: {
 },
 drawerItem: {
   ...theme.typography.tab,
-  color: "white"
+  color: "white",
+  opacity: 0.7
 },
 drawerItemEstimate: {
   backgroundColor: theme.palette.common.orange
