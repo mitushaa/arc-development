df1d49c8808ff90b008c19f085f470c67f523f41
mitushaa <aryamitusha@gmail.com>
2021-01-11 20:09:58 +0530

    Section Cleanup- Mounted

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -225,6 +225,7 @@ const routes = [
       classes={{paper: classes.menu}}
       MenuListProps={{onMouseLeave: handleClose}}
       elevation={0}
+      keepMounted
       >
       {menuOptions.map((option, i) => (
 <MenuItem key= {option}component = {Link} to ={option.link}
@@ -255,61 +256,6 @@ classes={{root: classes.menuItem}} onClick={(event) =>
 </ListItem>
   
   ))}
-  <ListItem 
-  onClick={() => {setOpenDrawer(false); 
-  setValue(0)
-}} 
-  divider 
-  button 
-  component={Link} 
-  to="/"
-  selected={value === 0}
-  >
-
-  <ListItemText className= {
-    value === 0 ?
-  [classes.drawerItem, classes.drawerItemSelected] 
-  :classes.drawerItem}
-  disableTypography
-  >
-    Home</ListItemText>
-  </ListItem>
-  <ListItem onClick={() => {setOpenDrawer(false); setValue(1)}} 
-  divider button component={Link} to="/services"
-    selected={value === 1}
-    >
-    <ListItemText className= {value === 1 ?
-  [classes.drawerItem, classes.drawerItemSelected] :
-  classes.drawerItem}
-    disableTypography>Services</ListItemText>
-  </ListItem>
-  <ListItem onClick={() => {setOpenDrawer(false); setValue(2)}} 
-  divider button component={Link} to="/revolution"
-    selected={value === 2}
-    >
-    <ListItemText className= {value === 2 ?
-  [classes.drawerItem, classes.drawerItemSelected] :
-  classes.drawerItem}
-    disableTypography>The Revolution</ListItemText>
-  </ListItem>
-  <ListItem onClick={() => {setOpenDrawer(false); setValue(3)}} 
-  divider button component={Link} to="/about"
-    selected={value === 3}
-    >
-    <ListItemText className=  {value === 3 ?
-  [classes.drawerItem, classes.drawerItemSelected] :
-  classes.drawerItem}
-    disableTypography>About Us</ListItemText>
-  </ListItem>
-  <ListItem onClick={() => {setOpenDrawer(false); setValue(4)}}  
-  divider button component={Link} to="/contact"
-    selected={value === 4}
-    >
-    <ListItemText className= {value === 4 ?
-  [classes.drawerItem, classes.drawerItemSelected] :
-  classes.drawerItem}
-    disableTypography>Contact Us</ListItemText>
-  </ListItem>
   <ListItem onClick={() => {setOpenDrawer(false); setValue(5)}} 
   divider button 
   component={Link} 
