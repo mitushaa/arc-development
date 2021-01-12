1caece111a7fe3e9820a7798c51246ffb56e8401
mitushaa <aryamitusha@gmail.com>
2021-01-12 11:52:22 +0530

    Footer Setup - Importing footer logo

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import footerAdornment from '../../assets/Footer Adornment.svg'
 
 const useStyles = makeStyles(theme => ({
     footer: {
@@ -11,6 +12,10 @@ const useStyles = makeStyles(theme => ({
 export default function Footer() {
     const classes = useStyles();
     
-    return <footer className={classes.footer}>Example Footer</footer>;
+    return (
+    <footer className={classes.footer}>
+        <img alt="black decorative slash" src={footerAdornment} />
+   </footer>
+   );
 
 }
