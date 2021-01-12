acf1ea65c0128c84c23490132b063f44f1bb9062
mitushaa <aryamitusha@gmail.com>
2021-01-12 14:39:36 +0530

    Using Footers- Adding Links

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import {Link} from 'react-router-dom'
 import {makeStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
@@ -44,42 +45,39 @@ export default function Footer() {
         <Grid container justify="center" className= {classes.mainContainer}>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column" spacing={2}>             
-            <Grid item className={classes.link}>
+            <Grid item component={Link} to ="/" className={classes.link}>
                 Home
                 </Grid>
             </Grid>
             </Grid>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column" spacing={2}>             
-            <Grid item className={classes.link}>
+            <Grid item component={Link} to ="/services" className={classes.link}>
                 Services
                 </Grid>
-                <Grid item className={classes.link}>
-                Services
-                </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/customsoftware" className={classes.link}>
                 Custom Software Developement
                 </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/mobileapps" className={classes.link}>
                 Mobile App Development
                 </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/websites" className={classes.link}>
                 Website Development
                 </Grid>
             </Grid>
             </Grid>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column"spacing={2}> 
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/revolution" className={classes.link}>
                     The Revolution
                 </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/revolution" className={classes.link}>
                     Vision
                 </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/revolution" className={classes.link}>
                     Technology
                 </Grid>
-                <Grid item className={classes.link}>
+                <Grid item component={Link} to ="/revolution"  className={classes.link}>
                     Process
                 </Grid>
                 </Grid>
@@ -87,13 +85,13 @@ export default function Footer() {
 
                 <Grid item className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
-                        <Grid item className={classes.link}>
+                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
                             About Us
                         </Grid>
-                        <Grid item className={classes.link}>
+                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
                             History
                         </Grid>
-                        <Grid item className={classes.link}>
+                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
                             Team
                         </Grid>
                     </Grid>
@@ -101,7 +99,7 @@ export default function Footer() {
                     
 <Grid item className={classes.gridItem}>
     <Grid container direction="column" spacing={2}>
-        <Grid item className= {classes.link}>
+        <Grid item component={Link} to ="/contactus" className= {classes.link}>
             Contact Us
         </Grid>
     </Grid>
