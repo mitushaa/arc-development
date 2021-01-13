22e698e706126f49a7327f468545580682db7377
mitushaa <aryamitusha@gmail.com>
2021-01-13 22:39:15 +0530

    Responsive Grids- Import Hidden

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Link} from 'react-router-dom'
 import {makeStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Hidden from '@material-ui/core/Hidden'
 
 import footerAdornment from '../../assets/Footer Adornment.svg'
 
@@ -38,47 +39,69 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
-export default function Footer() {
+export default function Footer(props) {
     const classes = useStyles();
     
     return (
     <footer className={classes.footer}>
+     <Hidden mdDown>
         <Grid container justify="center" className= {classes.mainContainer}>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column" spacing={2}>             
-            <Grid item component={Link} to ="/" className={classes.link}>
+            <Grid item component={Link} onClick={()=> props.setValue(0)} to ="/" className={classes.link}>
                 Home
                 </Grid>
             </Grid>
             </Grid>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column" spacing={2}>             
-            <Grid item component={Link} to ="/services" className={classes.link}>
+            <Grid item component={Link} onClick={()=> props.setValue(1)} 
+            to ="/services" className={classes.link}>
                 Services
                 </Grid>
-                <Grid item component={Link} to ="/customsoftware" className={classes.link}>
+                <Grid item component={Link} onClick={()=> props.setValue(1)}
+ to ="/customsoftware" className={classes.link}
+          
+                >
                 Custom Software Developement
                 </Grid>
-                <Grid item component={Link} to ="/mobileapps" className={classes.link}>
+                <Grid item component={Link} onClick={()=> props.setValue(1)}
+ to ="/mobileapps" className={classes.link}
+                
+                >
                 Mobile App Development
                 </Grid>
-                <Grid item component={Link} to ="/websites" className={classes.link}>
+                <Grid item component={Link} onClick={()=> props.setValue(1)}
+                to ="/websites" className={classes.link}                >
                 Website Development
                 </Grid>
             </Grid>
             </Grid>
             <Grid item className={classes.gridItem}>
                 <Grid container direction="column"spacing={2}> 
-                <Grid item component={Link} to ="/revolution" className={classes.link}>
+                <Grid item component={Link} 
+                to ="/revolution" 
+                className={classes.link}
+                onClick={()=> props.setValue(2)}
+                >
                     The Revolution
                 </Grid>
-                <Grid item component={Link} to ="/revolution" className={classes.link}>
+                <Grid item component={Link} to ="/revolution" className={classes.link}
+                onClick={()=> props.setValue(2)}
+
+                >
                     Vision
                 </Grid>
-                <Grid item component={Link} to ="/revolution" className={classes.link}>
+                <Grid item component={Link} to ="/revolution" className={classes.link}
+                onClick={()=> props.setValue(2)}
+
+                >
                     Technology
                 </Grid>
-                <Grid item component={Link} to ="/revolution"  className={classes.link}>
+                <Grid item component={Link} to ="/revolution"  className={classes.link}
+                onClick={()=> props.setValue(2)}
+
+                >
                     Process
                 </Grid>
                 </Grid>
@@ -86,13 +109,14 @@ export default function Footer() {
 
                 <Grid item className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
-                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
+                        <Grid item onClick={()=> props.setValue(3)}  component={Link} to ="/about" className={classes.link}
+                        >
                             About Us
                         </Grid>
-                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
+                        <Grid item onClick={()=> props.setValue(3)}  component={Link} to ="/about" className={classes.link}>
                             History
                         </Grid>
-                        <Grid item component={Link} to ="/aboutus" className={classes.link}>
+                        <Grid item onClick={()=> props.setValue(3)}  component={Link} to ="/about" className={classes.link}>
                             Team
                         </Grid>
                     </Grid>
@@ -100,15 +124,16 @@ export default function Footer() {
                     
 <Grid item className={classes.gridItem}>
     <Grid container direction="column" spacing={2}>
-        <Grid item component={Link} to ="/contactus" className= {classes.link}>
+        <Grid item onClick={()=> props.setValue(4)} component={Link} to ="/contact" className= {classes.link}>
             Contact Us
         </Grid>
     </Grid>
 </Grid>
+
                 </Grid>
 
 
-           
+                </Hidden>
         <img 
         alt="black decorative slash" 
         src={footerAdornment}
