1df0cf1d2341d3e310e4254d19b14d06b862fd1b
mitushaa <aryamitusha@gmail.com>
2021-01-14 10:43:04 +0530

    Animation to left

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -29,7 +29,7 @@ export default function LandingPage() {
     return (
         <Grid container direction="column">
             <Grid item>
-            <Grid container direction="row">
+            <Grid container justify= "flex-end" direction="row">
             <Grid item>
                 <div>Bringing the West Coast Technology<br/>to the Midwest</div>
                 <Grid container>
